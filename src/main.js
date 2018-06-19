bc9ed31c74a70788064dccd82c7d7fd6a838ad7c
Martti Raavel <mrt@mrt.ee>
2018-06-19 17:16:56 +0300

    Remove some packages

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
 
 import { store } from './store/store';
 import VueFlashMessage from 'vue-flash-message';
 
 Vue.use(VueFlashMessage);
-Vue.use(VueResource);
 Vue.use(require('vue-moment'));
 
 new Vue({
